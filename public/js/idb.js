9583191989fc9586ba2fbca39a941a040d9f5d8f
Jiyeon31 <zhdzhddlx@gmail.com>
2022-05-02 03:44:26 -0400

    deployed mongoDB and heroku

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,8 +1,13 @@
+// create variable to hold db connection
 let db;
+// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
 const request = indexedDB.open('budget-manager', 1);
 
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function(event) {
+  // save a reference to the database 
   const db = event.target.result;
+  // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
   db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
@@ -21,9 +26,12 @@ request.onerror = function(event) {
   console.log(event.target.errorCode);
 };
 
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
 function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions 
   const transaction = db.transaction(['new_transaction'], 'readwrite');
 
+  // access the object store for `new_pizza`
   const budgetObjectStore = transaction.objectStore('new_transaction');
 
   // add record to your store with add method.
